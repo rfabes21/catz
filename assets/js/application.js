45f1b45abf112c2d1abdef4d8d82e7bd6bee0685
Ryan <rfabester@gmail.com>
2014-01-28 16:48:46 -0800

    after fixes

--- assets/js/application.js
+++ assets/js/application.js
@@ -10,8 +10,9 @@ var AngryCats = Backbone.Collection.extend ({
     model: AngryCat,
 
     initialize: function(cats) {
+
         var rank = 1;
-        _.each(function(cat) {
+        _.each(cats, function(cat) {
             cat.rank = rank;
             ++rank;
         });
@@ -24,8 +25,8 @@ var AngryCatView = Backbone.Marionette.ItemView.extend({
     className: "angry_cat",
 
     events: {
-        'click .rank_up img': rankUp,
-        'click .rank_down img': rankDown
+        'click .rank_up img': 'rankUp',
+        'click .rank_down img': 'rankDown'
     },
 
     rankUp: function() {
@@ -42,31 +43,32 @@ var AngryCatView = Backbone.Marionette.ItemView.extend({
     id: "angry_cats",
     className: "table-striped table-bordered",
     template: "#angry_cats-template",
-    itemView: "AngryCatView",
+    itemView: AngryCatView,
+    itemViewContainer: 'tbody',
 
     initialize: function(){
-       this.listenTo(this.collection, "sort", this.renderCollection);
-    },
 
-    appendHTML: function(collectionView, itemView){
-        collectionView.$("tbody").append(itemView.el);
+       this.listenTo(this.collection, "sort", this.renderCollection);
     }
 });
 
-MyApp.addInitializer = function(options){
-    var angryCatsView = new AngryCatsView({
-        collection: options.cats
-    });
-    MyApp.mainRegion.show(angryCatsView);
-};
+// MyApp.addInitializer = function(options){
+//     var angryCatsView = new AngryCatsView({
+//         collection: options.cats
+//     });
+//     MyApp.mainRegion.show(angryCatsView);
+// };
 
 $(document).ready( function(){
     var cats = new AngryCats([
-        { name: "Wet Cat", image_path: "/assets/images/cat1.jpg" },
-        { name: "Bitey Cat", image_path: "/assets/images/cat2.jpg" },
+        { name: "Wet Cat", image_path: "/assets/images/cat2.jpg" },
+        { name: "Bitey Cat", image_path: "/assets/images/cat1.jpg" },
         { name: "Surprised Cat", image_path: "/assets/images/cat3.jpg" }
     ]);
-    MyApp.start({cats: cats});
+    var angryCatsView = new AngryCatsView({
+        collection: cats
+    });
+    MyApp.mainRegion.show(angryCatsView);
 });
 
 
